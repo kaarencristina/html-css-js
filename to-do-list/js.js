d22b2c5f486356c2ed6b12cbf17c9260ce020a9b
Karen Cristina <karen-kaah@hotmail.com>
2024-10-25 16:53:52 +0200

    js

--- to-do-list/js.js
+++ to-do-list/js.js
@@ -1,10 +1,12 @@
 const button=document.querySelector('.btn-task')
 const input=document.querySelector('.input-task')
+const listaCompleta.document.querySelector('.list-tasks')
 
 let myList=[]  
 
 function AddTarefa(){
-    myList.push(input.value)
+    myList.push({tarefa:input.value,concluida:false})
+    input.value=''
     mostrarTarefas()
 }
 
@@ -14,17 +16,47 @@ function mostrarTarefas(){
 
     let novaLi=''
 
-    myList.forEach()
+    myList.forEach((item, posicao)=>{
+
+        novaLi= novaLi+`
+        <li class="task" class="${item.concluida && "done"}">
+                    <img src="assets/checked.png" alt="check" onclick="Concluir(${posicao})" />
+                    <p>${item.tarefa}</p>
+                    <img src="assets/trash.png" alt="trash" onclick="deletarItem(${posicao})" />
+                    </li>
+                    
+            `
 
 
+    })
 
+    listaCompleta.innerHTML=novaLi
 
-
-    /*<li class="task">
-                    <img src="assets/checked.png" alt="check" />
-                    <p>dar like</p>
-                    <img src="assets/trash.png" alt="trash" />
-                    </li>*/
+    localStorage.setItem('lista',JSON.stringify(myList))
 }
 
+
+function recarregarTela(){
+    const tarefasdolocal=localStorage.getItem('lista')
+    myList=JSON.parse(tarefasdolocal)
+
+    if(tarefasdolocal){
+    mostrarTarefas()}
+}
+
+function Concluir(){
+
+ myList[posicao].concluida=!myList[posicao].concluida
+ mostrarTarefas()
+}
+
+
+function deletarItem(posicao){
+    myList.splice(posicao,1)
+
+    mostrarTarefas()
+
+}
+   
+recarregarTela()
 button.addEventListener('click', AddTarefa)
